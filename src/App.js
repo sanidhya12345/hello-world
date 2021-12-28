8ac4e2a5de9a62d5a32a6c20b15c656e1e7a7757
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2021-12-28 11:26:51 +0530

    first-react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Hi,I am Sanidhya Varshney</h1>
+       <p>This is my first React App.....</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
